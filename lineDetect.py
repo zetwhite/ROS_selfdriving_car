80ea04d25c29067a99ce33548fee36a38286559b
zetwhite <zetwhite@naver.com>
2020-08-05 14:02:05 -0700

    push line bipartite matching!

--- lineDetect.py
+++ lineDetect.py
@@ -4,16 +4,39 @@ import math
 import matplotlib.pyplot as plt
 import glob
 
+class Line : 
+    def __init__(self, start, end) : 
+        self.start = start
+        self.end = end
+    def update(self, start, end) : 
+        self.start = start 
+        self.end = end
+
 cap = cv2.VideoCapture('./track-s.mkv')
+width = 0
+height = 0
+fig, ax = plt.subplots(2, 2)
+
+lineSetted = False 
+lines= [Line(0, 0) , Line(0, 0), Line(0, 0)] 
+#leftLine = Line(0, 0)
+#midLine = Line(0, 0)
+#rightLine = Line(0, 0)
+
+
+def init() : 
+    global cap, width, height, fig, ax, lineSetted
+    cap = cv2.VideoCapture('./track-s.mkv')
+    ret, frame = cap.read() 
+    height, width = frame.shape[:2]
+    plt.ion()
 
 def canny(image) : 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    blur = cv2.GaussianBlur(image, (5, 5), 0)
     canny = cv2.Canny(blur, 50, 150)
     return canny
 
 def region_of_interest(image): 
-    height, width = image.shape[:2] 
     region = np.array([[(0, height - 30), (width, height - 30), (width - 200, 250), (200, 250)]])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, region, 255)
@@ -21,30 +44,92 @@ def region_of_interest(image):
     return masked_image
 
 def perspection(image):
-    height, width = image.shape[:2]
-    pts1 = np.float32([[width/5, height/2], [width/5 * 4, height/2], [0, height], [width, height]])
+    pts1 = np.float32([[width//5, height//2], [width//5 * 4, height//2], [0, height], [width, height]])
     pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
     M = cv2.getPerspectiveTransform(pts1, pts2)
     perspection_image = cv2.warpPerspective(image, M, (width, height))
     return perspection_image
 
+def get_histogram(image):
+    cuttedImage = image[height//3:, :]
+    ret, image_bin = cv2.threshold(cuttedImage, 125, 255, cv2.THRESH_BINARY)
+    hist = np.sum(image_bin, axis = 0)
+    return hist
+
+def imageShow(img1, img2, img3, img4) : 
+    ax[0, 0].imshow(img1, cmap='gray', vmin = 0, vmax = 255)
+    ax[0, 1].imshow(img2, cmap='gray', vmin = 0, vmax = 255)
+    ax[1, 0].imshow(img3, cmap='gray', vmin = 0, vmax = 255)
+    ax[1, 1].plot(img4, color = 'b')
+    plt.pause(0.0001)
+    ax[1, 1].lines.pop(0)
+    plt.show(False)
+    plt.draw()
+
+def getLine(histogram) : 
+    global lineSetted, lines
+    start = []
+    end = [] 
+    thres = 100
+    prev = histogram[0] 
+    for i in range(1, len(histogram)) : 
+        now = histogram[i]
+        if prev < thres and now > thres : 
+            start.append(i) 
+        if prev > thres and now < thres : 
+            end.append(i)
+        prev = now 
+    print("start : ", start)
+    print("end : " , end)
+    
+    if len(start) != len(end) : 
+        return
+    if lineSetted == False and len(start) == 3 : 
+        lineSetted = True
+        for i in range(3) : 
+            lines[i].update(start[i], end[i]) 
+        return 
+
+    #do bipartite matching!!!
+    if lineSetted == True and len(start) == 1 : 
+        minIndex = 0 
+        mindiff = 10000
+        for i in range(3) : 
+            diff = abs(lines.start - start[i]) + abs(lines.end - end[i]) 
+            if( diff < mindiff) : 
+                mindiff = diff 
+                minIndex = i 
+        lines[i].update(start[0], end[0])
+
+    if lineSetted == True and len(start) == 2 : 
+        minIndex = set()
+        mindiff = 10000 
+        for i in range(3) : 
+            for j in (set(range(3)) - set(i)) : 
+                if dif < mindiff : 
+                    mindiff = diff 
+                    #minIndex = setrange((30) - set(i))
+
+
+
+init()
 while (cap.isOpened) : 
     ret, frame = cap.read()
     
     if ret : 
-        #cv2.imshow("video", frame)
-        cannyed_image = canny(frame) 
-        cv2.imshow("cannyed", cannyed_image)
+        grayframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("gray", grayframe)
+        cannyed_image = canny(grayframe) 
         cropped_image = region_of_interest(cannyed_image)
-        cv2.imshow("cropped", cropped_image)
         topview_image = perspection(cropped_image)
-        cv2.imshow("topview", topview_image)
-        
-        k = cv2.waitKey(33)
+        hist = get_histogram(topview_image) 
+        getLine(hist)
+        imageShow(cannyed_image, cropped_image, topview_image, hist)
+        k = cv2.waitKey(1)
         if k == 27 : 
             break
     else : 
             break
-        
+#plt.show() 
 cap.release() 
 cv2.destroyAllWindows()
